3482162a61307e555b1022eb35048f4d04dbc68b
tukangzheng <tukangzheng@163.com>
2018-02-09 00:01:30 +0800

    '修改Request对象'

--- lexus-rpc/src/main/java/org/lexus/rpc/LRequest.java
+++ lexus-rpc/src/main/java/org/lexus/rpc/LRequest.java
@@ -1,6 +1,8 @@
 package org.lexus.rpc;
 
 
+import java.util.Map;
+
 /**
  * Request
  * @author tukangzheng
@@ -33,5 +35,31 @@ public interface LRequest {
      * @return
      */
     long getRequestId();
+
+    /**
+     * get attachments.
+     *
+     * @return attachments.
+     * @serial
+     */
+    Map<String, String> getAttachments();
+
+
+    /**
+     * get attachment by key.
+     *
+     * @return attachment value.
+     * @serial
+     */
+    String getAttachment(String key);
+
+
+    /**
+     * get attachment by key with default value.
+     *
+     * @return attachment value.
+     * @serial
+     */
+    String getAttachment(String key, String defaultValue);
 
 }
