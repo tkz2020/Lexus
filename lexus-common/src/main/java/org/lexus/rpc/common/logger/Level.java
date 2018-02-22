2b7a5228ec824ccff37f9fc772a4a24ae9ce4366
tukangzheng <tukangzheng@gomeplus.com>
2018-02-22 18:26:01 +0800

    '添加日志接口'

--- lexus-common/src/main/java/org/lexus/rpc/common/logger/Level.java
+++ lexus-common/src/main/java/org/lexus/rpc/common/logger/Level.java
@@ -1,4 +1,44 @@
 package org.lexus.rpc.common.logger;
 
+
+/**
+ * @author tukangzheng
+ */
 public enum Level {
+
+    /**
+     * ALL
+     */
+    ALL,
+
+    /**
+     * TRACE
+     */
+    TRACE,
+
+    /**
+     * DEBUG
+     */
+    DEBUG,
+
+    /**
+     * INFO
+     */
+    INFO,
+
+    /**
+     * WARN
+     */
+    WARN,
+
+    /**
+     * ERROR
+     */
+    ERROR,
+
+    /**
+     * OFF
+     */
+    OFF
+
 }
